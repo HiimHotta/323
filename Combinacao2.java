a1eff3d998ef5ea89b2f82026ed30fb08870d16f
HiimHotta <daniel.y.hotta@gmail.com>
2019-02-24 20:25:59 -0300

    Add files via upload
    teste

--- Combinacao2.java
+++ Combinacao2.java
@@ -116,7 +116,7 @@ public class Combinacao2 {
 
         else if (posAtual < k - 1 && array[posAtual] < n - posAtual) {
           reset (array, posAtual, k);
-          for (int i = 0; i < k - posAtual; i++) {
+          for (int i = posAtual; i < n - posAtual; i++) {
             increment (array, posAtual, k);
             printComb (array, ++posAtual, n, k);
             //StdOut.println("\n\n");
