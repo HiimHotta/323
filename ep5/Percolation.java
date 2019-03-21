23ecc755294211e19af2d996e34c7bd3bbf30aef
HiimHotta <daniel.y.hotta@gmail.com>
2019-03-20 23:23:08 -0300

    Add files via upload

--- ep5/Percolation.java
+++ ep5/Percolation.java
@@ -1,7 +1,6 @@
 /****************************************************************
     Nome: Daniel Yoshio Hotta
     NUSP: 9922700
-
     Ao preencher esse cabeçalho com o meu nome e o meu número USP,
     declaro que todas as partes originais desse exercício programa (EP)
     foram desenvolvidas e implementadas por mim e que portanto não 
@@ -13,16 +12,13 @@
     disciplina.
     Entendo que EPs sem assinatura devem receber nota zero e, ainda
     assim, poderão ser punidos por desonestidade acadêmica.
-
     Descrição de ajuda ou indicação de fonte:
-
     Se for o caso, descreva a seguir 'bugs' e limitações do seu programa:
-
 ****************************************************************/
 
 /******************************************************************************
- *  Compilation:  javac-algs4 Permutation.java
- *  Execution:    java Permutation k
+ *  Compilation:  javac-algs4 Percolation.java
+ *  Execution:    java Percolation k
  *
  *  Por convencao, 0 significa bloqueado
  *
@@ -40,57 +36,90 @@ import java.lang.UnsupportedOperationException;
 
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
     private int n;
+    private WeightedQuickUnionUF uf;
     private byte grid[][];
 
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n) {
+    public Percolation (int n) {
       if (n <= 0)
         throw new IllegalArgumentException ("Digite n > 0.");
-      grid = new byte [n][n]
+
+      grid = new byte[n][n];
+      uf   = new WeightedQuickUnionUF (n * n);
+
       this.n = n;
     }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col) {
+    public void open (int row, int col) {
       if (row < 0 || row >= n || col < 0 || col >= n)
         throw new IllegalArgumentException ("Linha ou Coluna invalida.");
       grid[row][col] = 1;
+      connectGrid (row, col);
+    }
+
+    private void connectGrid (int row, int col) {
+      if (row - 1 >= 0)
+        if (grid[row - 1][col] == 1)
+          union (n * (row - 1) + col, n * row + col);
+      if (col - 1 >= 0)
+        if (grid[row][col - 1] == 1)
+          union (n * row + col - 1  , n * row + col);
+      if (row + 1 < n)
+        if (grid[row + 1][col] == 1)
+          union (n * (row + 1) + col, n * row + col);
+      if (col + 1 < n)
+        if (grid[row][col + 1] == 1)
+          union (n * row + col + 1  , n * row + col);
     }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col) {
+    public boolean isOpen (int row, int col) {
       if (row < 0 || row >= n || col < 0 || col >= n)
         throw new IllegalArgumentException ("Linha ou Coluna invalida.");
       return grid[row]col] > 0;
     }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col) {
+    public boolean isFull (int row, int col) {
       if (row < 0 || row >= n || col < 0 || col >= n)
         throw new IllegalArgumentException ("Linha ou Coluna invalida.");
       return grid[row][col] > 1;
     }
 
     // returns the number of open sites
-    public int numberOfOpenSites() {
-      int count;
-      for (int i = 0; i < n; i++) 
-        for (int j = 0; j < n; j++) {
-          if (grid[i][j] > 0)
-            count++;
-        }
+    public int numberOfOpenSites () {
+      return uf.count ();
     }
 
     // does the system percolate?
-    public boolean percolates() {
-
+    public boolean percolates () {
+      for (int i = 0; i < n; i++) 
+        for (int j = 0; j < n; j++) {
+          if (uf.connected (i, n * (n - 1) + j))
+            return true;
+        }
     }
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main (String[] args) {
+        int n = args[0];
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF (n);
+        while (!StdIn.isEmpty ()) {
+          int p = StdIn.readInt ();
+          int q = StdIn.readInt ();
+          if (uf.connected (p, q)) 
+            continue;
+          uf.union (p, q);
+          StdOut.println (p + " " + q);
+        }
+        StdOut.println (uf.count () + " components");
+        StdOut.println ("Percolates:" + uf.percolates ())
+    }
 
 }
