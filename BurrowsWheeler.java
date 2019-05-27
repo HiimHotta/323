4481ded6ef66cd130cca54a4ecf1258ef412350a
HiimHotta <daniel.y.hotta@gmail.com>
2019-05-26 23:09:06 -0300

    queue

--- BurrowsWheeler.java
+++ BurrowsWheeler.java
@@ -12,74 +12,64 @@ public class BurrowsWheeler {
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output 
     public static void transform () {
-        while (!BinaryStdIn.isEmpty ()) {
-            String s = BinaryStdIn.readString ();
-            char[] t = s.toCharArray ();
-    	    CircularSuffixArray circular = new CircularSuffixArray (s);
+        String s = BinaryStdIn.readString ();
+        char[] t = s.toCharArray ();
+    	CircularSuffixArray circular = new CircularSuffixArray (s);
 
-
-            BinaryStdOut.write (3);
-            /*
-            //first
-            for (int i = 0; i < t.length; i++) {
-                if (circular.index (i) == 0) {
-                    StdOut.println (i);
-                    BinaryStdOut.write (i);
-                    break;
-                }
-            }
-*/
-            //t[]
-            for (int i = 0; i < t.length; i++) {
-                BinaryStdOut.write (t[(t.length - 1 + circular.index (i)) % t.length]);
+        //first
+        for (int i = 0; i < t.length; i++) {
+            if (circular.index (i) == 0) {
+                BinaryStdOut.write (i);
+                break;
             }
         }
+
+            //t[]
+        for (int i = 0; i < t.length; i++) 
+            BinaryStdOut.write (t[(t.length - 1 + circular.index (i)) % t.length]);
+            
         BinaryStdOut.flush ();
     }
 
     // apply Burrows-Wheeler inverse transform,
     // reading from standard input and writing to standard output
     public static void inverseTransform () {
-        while (!BinaryStdIn.isEmpty ()) {
-            int first = BinaryStdIn.readInt ();
-            String s = BinaryStdIn.readString ();
-            char[] t = s.toCharArray ();
-            int[] next = new int[t.length];
-            Queue <Integer> queue[] = new Queue[alphabetL];
+        int first = BinaryStdIn.readInt ();
+        String s = BinaryStdIn.readString ();
+        char[] t = s.toCharArray ();
+        int[] next = new int[t.length];
+        Queue <Integer> queue[] = new Queue[alphabetL];
 
             //read occurrences
-            for (int i = 0; i < t.length - 1; i++) {
+        for (int i = 0; i < t.length - 1; i++) {
+            if (queue[t[i]] == null)
                 queue[t[i]] = new Queue <Integer> ();
-                queue[t[i]].enqueue (i);
-            }
+            queue[t[i]].enqueue (i);
+        }
 
             //create next
-            int aux = 0;
-            for (int i = 0; i < alphabetL; i++) {
-                if (queue[i] != null) {
-                    while (!queue[i].isEmpty ())
-                        next[aux++] = queue[i].dequeue ();
-                }
-            }
-
-            aux = first;
-            for (int i = 1; i < t.length; i++) {
-
-                BinaryStdOut.write (t[aux]);
-                aux = next [aux];
+        int aux = 0;
+        for (int i = 0; i < alphabetL; i++) {
+            if (queue[i] != null) {
+                while (!queue[i].isEmpty ())
+                next[aux++] = queue[i].dequeue ();
             }
         }
-        
+
+        for (int i = 0; i < t.length; i++) {
+            BinaryStdOut.write (t[aux]);
+            aux = next [aux];
+        }
         BinaryStdOut.flush ();
     }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
     // if args[0] is "+", apply Burrows-Wheeler inverse transform
     public static void main (String[] args) {
-        if (args[0].equals ("+")) 
+        if (args[0].equals ("-")) 
             transform ();
 
-        if (args[0].equals ("-"))
+        if (args[0].equals ("+"))
             inverseTransform ();
     }
 
