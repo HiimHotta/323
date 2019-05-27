c3d70844fc9c545252a88b6b305055188a625785
HiimHotta <daniel.y.hotta@gmail.com>
2019-05-26 21:10:22 -0300

    Add files via upload

--- BurrowsWheeler.java
+++ BurrowsWheeler.java
@@ -1,17 +1,76 @@
+
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Quick;
+
 public class BurrowsWheeler {
+
+    public static int alphabetL = 256; //alphabet.length
 
     // apply Burrows-Wheeler transform,
     // reading from standard input and writing to standard output 
-    public static void transform() {
-    	
+    public static void transform () {
+        while (!BinaryStdIn.isEmpty ()) {
+            String s = BinaryStdIn.readString ();
+            char[] t = s.toCharArray ();
+    	    CircularSuffixArray circular = new CircularSuffixArray (s);
+
+            //first
+            for (int i = 0; i < s.length; i++) {
+                if (c.index (i) == 0)
+                    BinaryStdOut.write (i);
+            }
+
+            //t[]
+            for (int i = 0; i < s.length; i++) {
+                BinaryStdOut.write (t[(s.length - 1 + circular.index (i)) % s.length]);
+            }
+        }
+        BinaryStdOut.flush ();
+        BinaryStdOut.close ();
     }
 
     // apply Burrows-Wheeler inverse transform,
     // reading from standard input and writing to standard output
-    public static void inverseTransform()
+    public static void inverseTransform () {
+        while (!BinaryStdIn.isEmpty ()) {
+            int first = BinaryStdIn.readInt ();
+            String s = BinaryStdIn.readString ();
+            char[] t = s.toCharArray ();
+            int[] next = new int[s.length];
+            Queue[] queue = new Queue[alphabetL];
+
+            //read occurrences
+            for (int i = 0; i < s.length - 1; i++) 
+                queue[t[i]].enqueue (i);
+
+            //create next
+            int aux = 0;
+            for (int i = 0; i < alphabetL; i++) {
+                while (!queue[i].isEmpty ())
+                     next[aux++] = queue.dequeue ();
+            }
+
+            aux = first;
+            for (int i = 1; i < n; i++) {
+                BinaryStdOut.write (t[aux]);
+                aux = next [aux];
+            }
+        }
+        
+        BinaryStdOut.flush ();
+        BinaryStdOut.close ();
+    }
 
     // if args[0] is "-", apply Burrows-Wheeler transform
     // if args[0] is "+", apply Burrows-Wheeler inverse transform
-    public static void main(String[] args)
+    public static void main (String[] args) {
+        if (args[0].equals ("+")) 
+            transform ();
+
+        if (args[0].equals ("-"))
+            inverseTransform ();
+    }
 
 }
